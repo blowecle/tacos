874c5f97d17a7bb330d807f741c480f4411784ab
Brandon Lowe <BLoweCLE@gmail.com>
2023-01-05 21:19:45 -0600

    adjust construction page

--- src/Construction.js
+++ src/Construction.js
@@ -4,14 +4,15 @@ import './construction.css'
 const Construction = () => {
     return (
         <div className='construction-wrapper'>
-            <div className='title'>
-                Home of Tacos Taviesas, coming soon!
+            <div className='title text'>
+                Home of Tacos Traviesas, coming soon!
             </div>
-            <p className='p-tag'>In the meantime, checkout our instagram!</p>
-            <a href="https://www.instagram.com/tacostraviesas/" className="fa fa-instagram"></a>
             <div className='image-wrapper'>
                 <img className='construction-image' src='https://res.cloudinary.com/ddqp7dojc/image/upload/v1670023547/tacos/guinesstaco_tzufpw.jpg' alt='' />
             </div>
+            <p className='p-tag text'>In the meantime, checkout our instagram!</p>
+            <img className="arrow" src='https://media3.giphy.com/media/5QQ6FpAQ0syYLkONPB/giphy.gif'/>
+            <a href="https://www.instagram.com/tacostraviesas/" className="fa fa-instagram"></a>
         </div>
     )
 }

79fe6baf103741ee52a8f0b655e4feedb6ee9b17
Brandon Lowe <BLoweCLE@gmail.com>
2023-05-24 15:58:45 -0500

    mobile style

--- src/Construction.js
+++ src/Construction.js
@@ -4,15 +4,50 @@ import './construction.css'
 const Construction = () => {
     return (
         <div className='construction-wrapper'>
-            <div className='title text'>
-                Home of Tacos Traviesas, coming soon!
+            <div className='header-wrapper'>
+                <div className='title text'>
+                    Tacos Traviesas
+                </div>
             </div>
-            <div className='image-wrapper'>
-                <img className='construction-image' src='https://res.cloudinary.com/ddqp7dojc/image/upload/v1670023547/tacos/guinesstaco_tzufpw.jpg' alt='' />
+            <div className='blurb-wrapper'>
+                <div className='blurb'>Tacos Traviesas, An Authentic Mexican Taco Truck Offering Fresh-Made Corn Tortillas Daily</div>
             </div>
-            <p className='p-tag text'>In the meantime, checkout our instagram!</p>
-            <img className="arrow" src='https://media3.giphy.com/media/5QQ6FpAQ0syYLkONPB/giphy.gif'/>
-            <a href="https://www.instagram.com/tacostraviesas/" className="fa fa-instagram"></a>
+            <div className='slider'>
+                <div className='slides'>
+                    <div className='slide'>
+                        <img src='https://res.cloudinary.com/dyjzfdguj/image/upload/v1684951073/tacos/IMG_0223_sdsk2z.jpg' alt='taco'/>
+                    </div>
+                    <div className='slide'>
+                        <img src='https://res.cloudinary.com/dyjzfdguj/image/upload/v1684951073/tacos/IMG_0252_yvi6ju.jpg' alt='taco'/>
+                    </div>
+                    <div className='slide'>
+                        <img src='https://res.cloudinary.com/dyjzfdguj/image/upload/v1684951069/tacos/IMG_0244_d6cqrw.jpg' alt='taco'/>
+                    </div>
+                    <div className='slide'>
+                        <img src='https://res.cloudinary.com/dyjzfdguj/image/upload/v1684951071/tacos/IMG_0337_jrbpep.jpg' alt='taco'/>
+                    </div>
+                    <div className='slide'>
+                        <img src='https://res.cloudinary.com/dyjzfdguj/image/upload/v1684951069/tacos/IMG_0244_d6cqrw.jpg' alt='taco'/>
+                    </div>
+                </div>
+            </div>
+            <img className='tortillas-image' src='https://res.cloudinary.com/dyjzfdguj/image/upload/v1684938372/tacos/IMG_0942_r6a1vx.jpg' alt='tortillas'/>
+            <div className='tortilla-blurb'>
+            Discover taco perfection with our homemade corn tortillas at Tacos Traviesas. Made with fresh, stone-ground corn masa, these authentic gems are soft, flavorful, and pure taco bliss. Elevate your taste buds and experience the true essence of Mexican cuisine with our incredible homemade corn tortillas.
+            </div>
+            <img className='truck-image' src='https://res.cloudinary.com/dyjzfdguj/image/upload/v1684960996/tacos/70263335613__B49FC130-29C9-45F8-BE55-3D82F2B9DCC7_e3xsaq.jpg' alt='truck' />
+            <div className='truck-blurb'>
+            For more about what we've been doing and where we are popping up, check us out on:
+            </div>
+            <div className='socials'>
+                <a href='https://www.instagram.com/tacostraviesas/' target='_blank' rel='noreferrer'>Instagram</a>
+                <a href='https://twitter.com/tacostraviesas1' target='_blank' rel='noreferrer'>Twitter</a>
+            </div>
+
+
+
+
+
         </div>
     )
 }

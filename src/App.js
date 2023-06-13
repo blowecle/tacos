00b76ed98887ca2bf15e583b4e15a6f0082ac6f4
Brandon Lowe <BLoweCLE@gmail.com>
2023-06-13 14:52:26 -0400

    menu and further styling tweaks.  Ready for review #2

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
   const handleClick = (ref) => {
     if (ref.current) {
-      const headerHeight = 0.15 * window.innerHeight;
+      const headerHeight = 0.17 * window.innerHeight;
       const elementTopOffset = ref.current.getBoundingClientRect().top;
       const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
   
@@ -45,8 +45,8 @@ function App() {
                 </div>
 
                 <div className="sidebar-contents">
-                  <div onClick={() => handleClick(ref1)}>Menu</div>
                   <div onClick={() => handleClick(ref2)}>About Us</div>
+                  <div onClick={() => handleClick(ref1)}>Menu</div>
                   <div onClick={() => handleClick(ref3)}>Catering</div>
                   <div onClick={() => handleClick(ref4)}>Info</div>
                 </div>
@@ -54,14 +54,25 @@ function App() {
         </div>
       <Header/>
       <div className='logo text'>Tacos Traviesas</div>
-      <div className='content one'>
-        <div ref={ref1} className='content-header'>MENU</div>
-        <div className='content-body'>Etiam nec ipsum libero. Maecenas eu mi iaculis, blandit magna nec, pretium augue. Donec ut laoreet mauris. Praesent varius nibh mauris, at auctor neque sodales eget. Vestibulum lacus ipsum, posuere a maximus at, luctus et odio. Nunc eget elit libero. Etiam purus libero, efficitur et sem in, feugiat tempus lectus. Phasellus et pharetra risus.</div>
-        </div>
-      <img className='tortillas image' src='https://res.cloudinary.com/dyjzfdguj/image/upload/v1686322908/tacos/taco_hand_tpuhpn.jpg' alt='tortillas'/>
       <div className='content two'>
         <div ref={ref2} className='content-header'>ABOUT US</div>
-        <div className='content-body'>Morbi dignissim, augue gravida viverra feugiat, orci eros malesuada justo, luctus tincidunt magna lacus sit amet massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque interdum ipsum in nulla iaculis, a euismod diam cursus. Duis a tellus in massa vestibulum pulvinar vitae a arcu. Etiam eget dictum felis. Duis ornare pulvinar elementum. Phasellus aliquet turpis nisl, id auctor augue porttitor in. Morbi pharetra volutpat tempus. Ut facilisis cursus nulla at pulvinar. Maecenas dignissim auctor tortor in viverra. Vivamus porta velit quis purus rhoncus, vel congue lorem pellentesque.</div>
+        <div className='content-body-about'>Morbi dignissim, augue gravida viverra feugiat, orci eros malesuada justo, luctus tincidunt magna lacus sit amet massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque interdum ipsum in nulla iaculis, a euismod diam cursus. Duis a tellus in massa vestibulum pulvinar vitae a arcu. Etiam eget dictum felis. Duis ornare pulvinar elementum. Phasellus aliquet turpis nisl, id auctor augue porttitor in. Morbi pharetra volutpat tempus. Ut facilisis cursus nulla at pulvinar. Maecenas dignissim auctor tortor in viverra. Vivamus porta velit quis purus rhoncus, vel congue lorem pellentesque.</div>
+      </div>
+      <img className='tortillas image' src='https://res.cloudinary.com/dyjzfdguj/image/upload/v1686322908/tacos/taco_hand_tpuhpn.jpg' alt='tortillas'/>
+      <div className='content one'>
+        <div ref={ref1} className='content-header'>MENU</div>
+        <div className='menu-item'>Cochinita Pibil</div>
+        <div className='item-description'>Achiote Braised Pork Belly, Pickled Onion, Radish</div>
+        <div className='menu-item'>Barbacoa</div>
+        <div className='item-description'>Braised Beef Brisket in Adobo, Onion, Cilantro, Radish</div>
+        <div className='menu-item'>Roasted Cauliflower</div>
+        <div className='item-description'>with Smokey Chipotle, Radish</div>
+        <div className='menu-item'>Chicken or Bean Tostada</div>
+        <div className='item-description'>Black Bean, Cabbage, Radish, Onion, Cilantro, Crema, Cotija</div>
+        <div className='sides'>Sides</div>
+        <div className='menu-item'>{`Arroz (tasty rice!)`}</div>
+        <div className='menu-item'>{`Refried Beans (with Cotija)`}</div>
+        <div className='lime'>Everything Served with Lime!</div>
       </div>
       <div className='slider'>
         <div className='slides'>

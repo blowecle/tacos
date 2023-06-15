2ab95fecccc2cb996f6ee60a3969078681c329fa
Brandon Lowe <BLoweCLE@gmail.com>
2023-06-15 07:47:34 -0500

    next redesign

--- src/App.js
+++ src/App.js
@@ -33,76 +33,82 @@ function App() {
 
   return (
     <div className='wrapper'>
-    <div className='nav-wrapper'>
-          <div className={!visible ? 'nav-hamburger' : 'invisible'} onClick={() => setVisible(!visible)}>
-              <div className='nav-hamburger-line'></div>
-              <div className='nav-hamburger-line'></div>
-              <div className='nav-hamburger-line'></div>
-          </div>
-            <div className={visible ? "visible sidebar" : 'non-side' }>
-                <div className="sidebar-header">
-                    <h3>Welcome!</h3>
-                    <p id="x" onClick={() => setVisible(false)}>x</p>
-                </div>
+      <div className='inner-wrapper'>
+      <div className='nav-wrapper'>
+            <div className={!visible ? 'nav-hamburger' : 'invisible'} onClick={() => setVisible(!visible)}>
+                <div className='nav-hamburger-line'></div>
+                <div className='nav-hamburger-line'></div>
+                <div className='nav-hamburger-line'></div>
+            </div>
+              <div className={visible ? "visible sidebar" : 'non-side' }>
+                  <div className="sidebar-header">
+                      <h3>Welcome!</h3>
+                      <p id="x" onClick={() => setVisible(false)}>x</p>
+                  </div>
 
-                <div className="sidebar-contents">
-                  <div onClick={() => handleClick(ref2)}>About Us</div>
-                  <div onClick={() => handleClick(ref1)}>Menu</div>
-                  <div onClick={() => handleClick(ref3)}>Catering</div>
-                  <div onClick={() => handleClick(ref4)}>Info</div>
-                </div>
-            </div>
+                  <div className="sidebar-contents">
+                    <div onClick={() => handleClick(ref2)}>About Us</div>
+                    <div onClick={() => handleClick(ref1)}>Menu</div>
+                    <div onClick={() => handleClick(ref3)}>Catering</div>
+                    <div onClick={() => handleClick(ref4)}>Info</div>
+                  </div>
+              </div>
+          </div>
+        <Header/>
+        <div className='logo-wrapper'>
+          <img className='logo' src={tortillas} alt='tortillas'/>
         </div>
-      <Header/>
-      <div className='logo-wrapper'>
-        <img className='logo' src={tortillas} alt='tortillas'/>
-      </div>
-      <div className='content two'>
-        <div ref={ref2} className='content-header'>ABOUT US</div>
-        <div className='content-body-about'>Morbi dignissim, augue gravida viverra feugiat, orci eros malesuada justo, luctus tincidunt magna lacus sit amet massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque interdum ipsum in nulla iaculis, a euismod diam cursus. Duis a tellus in massa vestibulum pulvinar vitae a arcu. Etiam eget dictum felis. Duis ornare pulvinar elementum. Phasellus aliquet turpis nisl, id auctor augue porttitor in. Morbi pharetra volutpat tempus. Ut facilisis cursus nulla at pulvinar. Maecenas dignissim auctor tortor in viverra. Vivamus porta velit quis purus rhoncus, vel congue lorem pellentesque.</div>
-      </div>
-      <div className='content one'>
-        <div ref={ref1} className='content-header'>MENU</div>
-        <div className='menu-item'>Cochinita Pibil</div>
-        <div className='item-description'>Achiote Braised Pork Belly, Pickled Onion, Radish</div>
-        <div className='menu-item'>Barbacoa</div>
-        <div className='item-description'>Braised Beef Brisket in Adobo, Onion, Cilantro, Radish</div>
-        <div className='menu-item'>Roasted Cauliflower</div>
-        <div className='item-description'>with Smokey Chipotle, Radish</div>
-        <div className='menu-item'>Chicken or Bean Tostada</div>
-        <div className='item-description'>Black Bean, Cabbage, Radish, Onion, Cilantro, Crema, Cotija</div>
-        <div className='sides'>Sides</div>
-        <div className='menu-item'>{`Arroz (tasty rice!)`}</div>
-        <div className='menu-item'>{`Refried Beans (with Cotija)`}</div>
-        <div className='lime'>Everything Served with Lime!</div>
-      </div>
-      <div className='slider'>
-        <div className='slides'>
-            <div className='slide'>
-                <img src='https://res.cloudinary.com/dyjzfdguj/image/upload/v1684951073/tacos/IMG_0223_sdsk2z.jpg' alt='taco'/>
-            </div>
-            <div className='slide'>
-                <img src='https://res.cloudinary.com/dyjzfdguj/image/upload/v1684951073/tacos/IMG_0252_yvi6ju.jpg' alt='taco'/>
-            </div>
-            <div className='slide'>
-                <img src='https://res.cloudinary.com/dyjzfdguj/image/upload/v1684951069/tacos/IMG_0244_d6cqrw.jpg' alt='taco'/>
-            </div>
-            <div className='slide'>
-                <img src='https://res.cloudinary.com/dyjzfdguj/image/upload/v1684951071/tacos/IMG_0337_jrbpep.jpg' alt='taco'/>
-            </div>
-            <div className='slide'>
+        <div className='content about'>
+          <div className='content-wrapper'>
+            <div ref={ref2} className='content-header'>ABOUT US</div>
+            <div className='content-body-about'>Morbi dignissim, augue gravida viverra feugiat, orci eros malesuada justo, luctus tincidunt magna lacus sit amet massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque interdum ipsum in nulla iaculis, a euismod diam cursus. Duis a tellus in massa vestibulum pulvinar vitae a arcu. Etiam eget dictum felis. Duis ornare pulvinar elementum. Phasellus aliquet turpis nisl, id auctor augue porttitor in. Morbi pharetra volutpat tempus. Ut facilisis cursus nulla at pulvinar. Maecenas dignissim auctor tortor in viverra. Vivamus porta velit quis purus rhoncus, vel congue lorem pellentesque.</div>
+          </div>
+        </div>
+        <div className='content menu'>
+        <div className='slider'>
+          <div className='slides'>
+              <div className='slide'>
+                  <img src='https://res.cloudinary.com/dyjzfdguj/image/upload/v1684951073/tacos/IMG_0223_sdsk2z.jpg' alt='taco'/>
+              </div>
+              <div className='slide'>
+                  <img src='https://res.cloudinary.com/dyjzfdguj/image/upload/v1684951073/tacos/IMG_0252_yvi6ju.jpg' alt='taco'/>
+              </div>
+              <div className='slide'>
                   <img src='https://res.cloudinary.com/dyjzfdguj/image/upload/v1684951069/tacos/IMG_0244_d6cqrw.jpg' alt='taco'/>
-            </div>
+              </div>
+              <div className='slide'>
+                  <img src='https://res.cloudinary.com/dyjzfdguj/image/upload/v1684951071/tacos/IMG_0337_jrbpep.jpg' alt='taco'/>
+              </div>
+              <div className='slide'>
+                    <img src='https://res.cloudinary.com/dyjzfdguj/image/upload/v1684951069/tacos/IMG_0244_d6cqrw.jpg' alt='taco'/>
+              </div>
+          </div>
         </div>
-      </div>
-      <div className='content three'>
-        <div ref={ref3} className='content-header'>CATERING</div>
-          <div className='content-body'>This is where the information about catering goes.  We have lots of catering.  Even our catering has catering.  Tortillas?  You know it.  Best fucking tortillas in all of Ireland, dawg.</div>
-      </div>
-      <img className='zoe image' src='https://res.cloudinary.com/dyjzfdguj/image/upload/v1684960996/tacos/70263335613__B49FC130-29C9-45F8-BE55-3D82F2B9DCC7_e3xsaq.jpg' alt='zoe'/>
-      <div className='content frou'>
-        <div ref={ref4} className='content-header'>INFO</div>
-          <div className='content-body'>This is where we put some information about Zoe.  She is a baller chef and has an awesome tattoo that says 'fuck brunch' because, well, fuck brunch!</div>
+          <div ref={ref1} className='content-header'>MENU</div>
+          <div className='menu-item'>Cochinita Pibil</div>
+          <div className='item-description'>Achiote Braised Pork Belly, Pickled Onion, Radish</div>
+          <div className='menu-item'>Barbacoa</div>
+          <div className='item-description'>Braised Beef Brisket in Adobo, Onion, Cilantro, Radish</div>
+          <div className='menu-item'>Roasted Cauliflower</div>
+          <div className='item-description'>with Smokey Chipotle, Radish</div>
+          <div className='menu-item'>Chicken or Bean Tostada</div>
+          <div className='item-description'>Black Bean, Cabbage, Radish, Onion, Cilantro, Crema, Cotija</div>
+          <div className='sides'>Sides</div>
+          <div className='menu-item'>{`Arroz (tasty rice!)`}</div>
+          <div className='menu-item'>{`Refried Beans (with Cotija)`}</div>
+          <div className='lime'>Everything Served with Lime!</div>
+        </div>
+        <div className='content catering'>
+          <div ref={ref3} className='content-header'>CATERING</div>
+            <div className='content-body'>This is where the information about catering goes.  We have lots of catering.  Even our catering has catering.  Tortillas?  You know it.  Best fucking tortillas in all of Ireland, dawg.</div>
+        </div>
+        <div className='content zoe-content'>
+        <div className='zoe-wrapper'>
+          <img className='zoe' src='https://res.cloudinary.com/dyjzfdguj/image/upload/v1684960996/tacos/70263335613__B49FC130-29C9-45F8-BE55-3D82F2B9DCC7_e3xsaq.jpg' alt='zoe'/>
+        </div>
+          <div ref={ref4} className='content-header'>HOW TO GET YOUR TACOS</div>
+            <div className='content-body'>This is where we put some information about Zoe.  She is a baller chef and has an awesome tattoo that says 'fuck brunch' because, well, fuck brunch!</div>
+        </div>
       </div>
     </div>
   );

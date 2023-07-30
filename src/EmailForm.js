34dac3b54b66a2a10fcbfb8eaea2a4ddc73d92e0
Brandon Lowe <BLoweCLE@gmail.com>
2023-07-30 11:39:56 -0500

    m

--- src/EmailForm.js
+++ src/EmailForm.js
@@ -4,17 +4,17 @@ export default function EmailForm() {
 
 
       return (
-        <form data-netlify="true">
-    <input
-      type="email"
-      placeholder="Your email"
-      required
-    />
-    <textarea
-      placeholder="Your message"
-      required
-    ></textarea>
-    <button type="submit">Send</button>
-  </form>
+        <form method="POST" data-netlify="true">
+            <input
+            type="email"
+            placeholder="Your email"
+            required
+            />
+            <textarea
+            placeholder="Your message"
+            required
+            ></textarea>
+            <button type="submit">Send</button>
+        </form>
       );
 }

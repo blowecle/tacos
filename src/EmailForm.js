61d3c7721ec3eff9f9788f47fc317a8e902da28f
Brandon Lowe <BLoweCLE@gmail.com>
2023-07-30 11:36:44 -0500

    netlify

--- src/EmailForm.js
+++ src/EmailForm.js
@@ -4,7 +4,7 @@ export default function EmailForm() {
 
 
       return (
-        <form method="POST" data-netlify="true">
+        <form data-netlify="true">
     <input
       type="email"
       placeholder="Your email"

94f181432697be04b034def6a28955992cbe8a4c
Brandon Lowe <BLoweCLE@gmail.com>
2023-07-30 11:23:58 -0500

    m

--- src/EmailForm.js
+++ src/EmailForm.js
@@ -1,30 +1,22 @@
-import React, { useState } from 'react'
+
 
 export default function EmailForm() {
-    const [ email, setEmail ] = useState('');
-    const [ message, setMessage ] = useState('');
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-    
-      };
 
       return (
-        <form onSubmit={handleSubmit} netlify>
-          <input
-            type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            placeholder="Your email"
-            required
-          />
-          <textarea
-            value={message}
-            onChange={e => setMessage(e.target.value)}
-            placeholder="Your message"
-            required
-          />
-          <button type="submit">Send</button>
-        </form>
+        <form netlify>
+    <input
+      type="email"
+      value={email}
+      placeholder="Your email"
+      required
+    />
+    <textarea
+      value={message}
+      placeholder="Your message"
+      required
+    ></textarea>
+    <button type="submit">Send</button>
+  </form>
       );
 }
